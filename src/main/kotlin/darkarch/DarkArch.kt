de65f0fff38d5f4316e69921c6ae205caabd5d2b
toby7002 <144540995+toby7002@users.noreply.github.com>
2023-10-07 23:47:45 +0700

    Finish build settings file and set up Spotless

--- src/main/kotlin/darkarch/DarkArch.kt
+++ src/main/kotlin/darkarch/DarkArch.kt
@@ -4,7 +4,7 @@ import net.fabricmc.api.ModInitializer
 import org.slf4j.LoggerFactory
 
 object DarkArch : ModInitializer {
-    private val logger = LoggerFactory.getLogger("darkarch")
+	private val logger = LoggerFactory.getLogger("darkarch")
 
 	override fun onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
